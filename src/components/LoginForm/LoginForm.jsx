487c1d379930fe37eb07fe02e82bb0a42caece27
pnyekrasov <pnekrasov1910@gmail.com>
2023-09-27 02:44:33 +0300

    HW 2

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,28 +1,56 @@
 import { Formik, Field, Form } from 'formik';
+import { useState } from 'react';
+import { VscEyeClosed, VscEye } from 'react-icons/vsc';
+import { Ikon, PasswordForm } from './LoginForm.styled';
+import * as Yup from 'yup';
 
-export const LoginForm = () => (
-  <div>
-    <h1>Sign Up</h1>
-    <Formik
-      initialValues={{
-        email: '',
-        password: '',
-      }}
-      onSubmit={values => {}}
-    >
-      <Form>
-        <label>
-          Email
-          <Field name="email" placeholder="jane@acme.com" type="email" />
-        </label>
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .required('Name is required')
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    ),
+  password: Yup.string()
+    .required('Phone number is required')
+    .matches(
+      /\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}/,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    ),
+});
 
-        <label>
-          Password
-          <Field name="password" type="password" />
-        </label>
+export const LoginForm = () => {
+  const [visible, setvisible] = useState(false);
+  const handleShow = () => {
+    setvisible(!visible);
+  };
 
-        <button type="submit">Log in</button>
-      </Form>
-    </Formik>
-  </div>
-);
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        onSubmit={values => {}}
+      >
+        <Form>
+          <label>
+            Email
+            <Field name="email" type="email" placeholder="jane@acme.com" />
+          </label>
+
+          <PasswordForm>
+            Password
+            <Field name="password" type={visible ? 'text' : 'password'} />
+            <Ikon onClick={handleShow}>
+              {visible ? <VscEyeClosed /> : <VscEye />}
+            </Ikon>
+          </PasswordForm>
+
+          <button type="submit">Log in</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
---
+++ src/components/LoginForm/LoginForm.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const PasswordForm = styled.div`
+  position: relative;
+`;
+
+export const Ikon = styled.div`
+  position: absolute;
+  top: 4px;
+  left: 220px;
+  z-index: 100;
+  cursor: pointer;
+`;
