4cb9758b213a82c796b8f26551113f42f2331141
pnyekrasov <pnekrasov1910@gmail.com>
2023-10-02 01:35:45 +0300

    HW 10

--- src/redux/Auth/authSlice.jsx
+++ src/redux/Auth/authSlice.jsx
@@ -12,12 +12,11 @@ const authSlice = createSlice({
   extraReducers: builder =>
     builder
       .addCase(register.pending, (state, action) => state)
-      .addCase(
-        register.fulfilled,
-        (state, action) => state
-        // state.user = action.payload.user;
-        // state.token = action.payload.token;
-      )
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
       .addCase(register.rejected, (state, action) => state),
 });
 
